eb3278b606f83e1ffd07eaa11cdf6ccbc2cc1e95
stevengangano <stevengangano@yahoo.com>
2020-04-07 18:35:37 -0700

    Updates

---
+++ precache-manifest.5253f2775ec5a5d2718798e3d90ff5ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3072ccd017157a17b99ca5e7bdcb7212",
+    "url": "/stevengangano/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a1ca1eada7b293968c62",
+    "url": "/stevengangano/react-monsters-rolodex/static/css/main.bda11a98.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/stevengangano/react-monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stevengangano/react-monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1ca1eada7b293968c62",
+    "url": "/stevengangano/react-monsters-rolodex/static/js/main.93c780a4.chunk.js"
+  },
+  {
+    "revision": "b7b08332bd1bcbf56714",
+    "url": "/stevengangano/react-monsters-rolodex/static/js/runtime-main.650b282c.js"
+  }
+]);
